396e556b6c3d4cdf110cfff28e982cedecf5e92a
Lucas Wiebe-Dembowski <lwiebedembowski@gmail.com>
2025-12-01 18:04:12 -0600

    options.lua: create commands for common typos of various exit commands.

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,12 @@
 vim.opt.tabstop = 4
 vim.opt.number = true
 
+vim.api.nvim_create_user_command('WQ', 'wq', {})
+vim.api.nvim_create_user_command('Wq', 'wq', {})
+vim.api.nvim_create_user_command('W', 'w', {})
+vim.api.nvim_create_user_command('Qa', 'qa', {})
+vim.api.nvim_create_user_command('Q', 'q', {})
+
 -- Define your colorscheme preferences
 local filetype_colors = {
   markdown = "default",
