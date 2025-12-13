bcdfc93cdb0da17b9b228d5c36f601716ad21cca
Lucas Wiebe-Dembowski <lwiebedembowski@gmail.com>
2025-12-13 11:15:23 -0600

    init.lua: Add Comment plugin.

--- init.lua
+++ init.lua
@@ -3,11 +3,14 @@ require("options")
 vim.call('plug#begin')
 
 -- List your plugins here
-vim.fn['plug#']('nvim-treesitter/nvim-treesitter') -- improved syntax highlighting, prerequisite for render-markdown
+vim.fn['plug#']('nvim-treesitter/nvim-treesitter') -- syntax tree for things like highlighting, prerequisite for render-markdown
 vim.fn['plug#']('MeanderingProgrammer/render-markdown.nvim')
 vim.fn['plug#']('neovim/nvim-lspconfig')
+vim.fn['plug#']('numToStr/Comment.nvim')
 
 vim.call('plug#end')
+
+require('Comment').setup()
 
 vim.lsp.enable('clangd')
 
