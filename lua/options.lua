602e4f8904cf8efb56a0f86d1530292ec35900ee
Lucas Wiebe-Dembowski <lwiebedembowski@gmail.com>
2026-09-03 18:12:11 -0500

    options: Add Wqa and Wq commands

--- lua/options.lua
+++ lua/options.lua
@@ -25,7 +25,9 @@ end
 
 vim.api.nvim_create_user_command('WQ', 'wq', {})
 vim.api.nvim_create_user_command('Wq', 'wq', {})
+vim.api.nvim_create_user_command('Wqa', 'wqa', {})
 vim.api.nvim_create_user_command('W', 'w', {})
+vim.api.nvim_create_user_command('Wa', 'wa', {})
 vim.api.nvim_create_user_command('Qa', 'qa', {})
 vim.api.nvim_create_user_command('Q', 'q', {})
 
